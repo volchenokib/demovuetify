09397337232e27c1bec52a8d96132e45b2fd480a
Igor <igor.volchenok@econophysica.com>
2019-04-15 18:50:18 +0700

    first commit

--- src/store.js
+++ src/store.js
@@ -1,16 +1,89 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+	state: {
+		data: {
+			isLoding: false
+		},
 
-  },
-  mutations: {
+		form: {
+			isReset: false
+		},
 
-  },
-  actions: {
+		button: {
+			isDisable: false
+		}
+	},
+	getters: {
+		getDataState(state) {
+			return state.data.isLoding;
+		},
 
-  }
-})
+		formResetNeed(state) {
+			return state.form.isReset;
+		},
+
+		getButtonState(state) {
+			return state.button.isDisable;
+		}
+	},
+
+	mutations: {
+		API_DATA_PENDING(state) {
+			state.data.isLoding = true;
+			state.button.isDisable = true;
+		},
+
+		API_DATA_SUCCES(state, payload) {
+			state.data.isLoding = false;
+			state.button.isDisable = false;
+			state.form.isReset = payload;
+			// console.log('API_DATA_SUCCES', state.form.isReset);
+		},
+
+		API_DATA_FAILURE(state, error) {
+			state.data.isLoding = false;
+			state.button.isDisable = false;
+			console.log(error);
+		}
+	},
+
+	actions: {
+		downloadFile(store, payload) {
+			store.commit('API_DATA_PENDING');
+
+			return axios
+				.get(
+					'https://78.media.tumblr.com/tumblr_m39nv7PcCU1r326q7o1_500.png',
+					{
+						responseType: 'arraybuffer'
+					},
+					payload
+				)
+				.then(response => {
+					// server imitation
+					setTimeout(() => {
+						store.commit('API_DATA_SUCCES', true);
+						store.dispatch('forceFileDownload', response);
+					}, 5000);
+				})
+				.catch(error => {
+					store.commit('API_DATA_FAILURE', error);
+				});
+		},
+
+		forceFileDownload(response) {
+			const url = window.URL.createObjectURL(new Blob([response.data]));
+			console.log('response.data', response.data);
+			const link = document.createElement('a');
+			link.href = url;
+			link.setAttribute('download', 'file.png'); //or any other extension
+			document.body.appendChild(link);
+			link.click();
+		}
+	}
+});
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,52 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+const localVue = createLocalVue();
+localVue.use(Vuetify);
+localVue.use(Vuex);
+
+let store;
+let state;
+let actions;
+let mutations;
+let getters;
+
+actions = {
+	downloadFile: jest.fn(),
+	forceFileDownload: jest.fn()
+};
+
+store = new Vuex.Store({
+	state: {},
+	actions,
+	mutations,
+	getters
+});
+
+// describe('HelloWorld component', () => {
+// 	it('renders to match snapshot', () => {
+// 		const wrapper = shallowMount(HelloWorld, {
+// 			localVue,
+// 			store
+// 		});
+// 		expect(wrapper).toMatchSnapshot();
+// 	});
+// });
+
+describe('vue demo component', () => {
+	const wrapper = shallowMount(HelloWorld, {
+		localVue,
+		store
+	});
+
+	it('submit button exist', () => {
+		expect(wrapper.contains('#submitButton')).toBe(true);
+	});
+	it('action for downloading file', () => {
+		wrapper.find('.v-btn').trigger('click');
+		expect(actions.downloadFile).toHaveBeenCalled();
+		expect(actions.downloadFile).toHaveBeenCalledTimes(1);
+	});
+});
