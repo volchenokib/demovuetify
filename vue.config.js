f7c4e9b75b26618c5473eb70d8fececf0cfd72db
Igor Volchenok <checklogon@gmail.com>
2021-07-19 16:15:34 +0700

    set up continuous deployment

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	pluginOptions: {
+		s3Deploy: {
+			awsProfile: 'default',
+			endpoint:
+				'Override the default AWS endpoint with another e.g. DigitalOcean.',
+			region: 'us-west-1',
+			bucket: 'vueetify_demo',
+			createBucket: 'true',
+			staticHosting: 'true',
+			staticIndexPage: 'index.html',
+			staticErrorPage: 'index.html',
+			assetPath: 'dist',
+			assetMatch: '**',
+			deployPath: '/',
+			acl: 'public-read',
+			pwa: false,
+			pwaFiles: 'Comma-separated list of files to treat as PWA files',
+			enableCloudfront: false,
+			uploadConcurrency: 5,
+			gzip: 'true',
+			gzipFilePattern:
+				'**/*.{js,css,json,ico,map,xml,txt,svg,eot,ttf,woff,woff2}'
+		}
+	}
+};
